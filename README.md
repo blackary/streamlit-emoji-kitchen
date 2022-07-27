31679b89897208280d3d2812f990e6727097e30f
Zachary Blackwood <zachary@streamlit.io>
2022-07-27 15:28:24 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+[![Streamlit App](https://static.streamlit.io/badges/streamlit_badge_black_white.svg)](https://emoji-kitchen.streamlitapp.com))
+
+# Streamlit Emoji Kitchen
+
+A version of https://emoji.supply/kitchen/ rewritten in Python with Streamlit
+
+Allows you to pick pairs of emojis, and for some of them, see a new emoji-like image
+that is a combination of them.
+
+See https://jenniferdaniel.substack.com/p/introducing-emoji-kitchen- for more info
---
+++ streamlit_app.py
@@ -0,0 +1,107 @@
+import json
+from pathlib import Path
+
+import pandas as pd
+import requests
+import streamlit as st
+from requests.exceptions import RequestException
+
+st.set_page_config(page_title="Emoji Kitchen", page_icon="‍:stew:", layout="wide")
+
+st.title(":stew: ‍Emoji Kitchen")
+
+
+def emoji_url(code_point) -> str:
+    return f"https://raw.githubusercontent.com/googlefonts/noto-emoji/main/png/128/emoji_u${code_point}.png"
+
+
+def code_point_to_emoji(code_point) -> str:
+    cps = [int(hex, 16) for hex in code_point.split("-")]
+    emoji = "".join(chr(cp) for cp in cps)
+    return emoji
+
+
+@st.experimental_singleton
+def get_matches() -> pd.DataFrame:
+    return pd.read_parquet("matches.parquet")
+
+
+def mixmoji_url(code_point1: str, code_point2: str) -> str:
+    # date = "20201001"
+    matches = get_matches()
+
+    try:
+        match = matches[
+            ((matches["emoji1"] == code_point1) & (matches["emoji2"] == code_point2))
+            | ((matches["emoji1"] == code_point2) & (matches["emoji2"] == code_point1))
+        ].iloc[0]
+    except IndexError:
+        return "NO MATCH"
+
+    date = match["date"]
+    code_point1 = match["emoji1"]
+    code_point2 = match["emoji2"]
+
+    code_point1 = "-u".join(code_point1.split("-"))
+    code_point2 = "-u".join(code_point2.split("-"))
+
+    return f"https://www.gstatic.com/android/keyboard/emojikitchen/{date}/u{code_point1}/u{code_point1}_u{code_point2}.png"
+
+
+# Load dictionary from points.json
+points = json.loads(Path("points.json").read_text())
+
+
+N_COLS = 30
+
+columns = st.columns(30)
+
+
+if "clicked" not in st.session_state:
+    st.session_state["clicked"] = []
+
+
+for idx, point in enumerate(points):
+    emoji = code_point_to_emoji(point)
+    col = columns[idx % 30]
+    with col:
+        if st.button(emoji):
+            st.session_state["clicked"].append(point)
+            if len(st.session_state["clicked"]) > 2:
+                st.session_state["clicked"].pop(0)
+
+# st.write(st.session_state["clicked"])
+
+clicked = st.session_state["clicked"]
+
+
+if not clicked:
+    first = "?"
+    second = "?"
+    third = "?"
+
+elif len(clicked) == 1:
+    first = code_point_to_emoji(clicked[0])
+    second = "?"
+    third = "?"
+
+elif len(clicked) == 2:
+    combo_url = mixmoji_url(clicked[0], clicked[1])
+    first = code_point_to_emoji(clicked[0])
+    second = code_point_to_emoji(clicked[1])
+    third = combo_url
+
+col1, col2, col3, col4, col5, _ = st.columns([1, 1, 1, 1, 1, 15])
+
+col1.write(f"# {first}")
+col2.write(f"# +")
+col3.write(f"# {second}")
+col4.write("# =")
+if third == "?":
+    col5.write(f"# {third}")
+else:
+    try:
+        requests.head(third).raise_for_status()
+        col5.image(third, use_column_width=True)
+    except RequestException:
+        col5.write(f"## Not found")
