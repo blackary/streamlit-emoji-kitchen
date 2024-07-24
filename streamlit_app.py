0d109f1a439ac80dcc40c56b165020e49321c89f
Zachary Blackwood <zachary@streamlit.io>
2024-07-24 15:09:31 -0400

    Modernize

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,26 +6,30 @@ import requests
 import streamlit as st
 from requests.exceptions import RequestException
 
-st.set_page_config(page_title="Emoji Kitchen", page_icon="‍:stew:", layout="wide")
+st.set_page_config(page_title="Emoji Kitchen", page_icon=":stew:", layout="wide")
 
-st.title(":stew: ‍Emoji Kitchen")
+st.title(":stew: Emoji Kitchen")
 
 
 st.markdown(
     """
 <style>
-button {
+[data-testid="stVerticalBlock"]:has(.emoji-grid) button p {
     font-size: 3.5rem;
     border: none !important;
 }
 
-.block-container div:first-child div:first-child {
+[data-testid="stVerticalBlock"]:has(.emoji-grid) {
     display: inline-block !important;
 }
 
-.element-container {
+[data-testid="stVerticalBlock"]:has(.emoji-grid) .element-container {
     display: inline-block !important;
     width: 80px;
+}
+
+[data-testid="stVerticalBlock"]:has(.emoji-grid) .stButton {
+    width: 0 !important;
 }
 </style>
 """,
@@ -33,6 +37,7 @@ button {
 )
 
 
+@st.cache_data
 def emoji_url(code_point) -> str:
     return f"https://raw.githubusercontent.com/googlefonts/noto-emoji/main/png/128/emoji_u${code_point}.png"
 
@@ -43,11 +48,12 @@ def code_point_to_emoji(code_point) -> str:
     return emoji
 
 
-@st.experimental_singleton
+@st.cache_resource
 def get_matches() -> pd.DataFrame:
     return pd.read_parquet("matches.parquet")
 
 
+@st.cache_data
 def mixmoji_url(code_point1: str, code_point2: str) -> str:
     # date = "20201001"
     matches = get_matches()
@@ -74,19 +80,19 @@ def mixmoji_url(code_point1: str, code_point2: str) -> str:
 points = json.loads(Path("points.json").read_text())
 
 if "clicked" not in st.session_state:
-    st.session_state["clicked"] = []
+    st.session_state["clicked"] = st.query_params.get_all("clicked")
 
 clicked = st.session_state["clicked"]
 
 if not clicked:
     first = "?"
     second = "?"
-    third = "?"
+    third = "❌"
 
 elif len(clicked) == 1:
     first = code_point_to_emoji(clicked[0])
     second = "?"
-    third = "?"
+    third = "❌"
 
 elif len(clicked) == 2:
     combo_url = mixmoji_url(clicked[0], clicked[1])
@@ -94,25 +100,51 @@ elif len(clicked) == 2:
     second = code_point_to_emoji(clicked[1])
     third = combo_url
 
-_, col1, col2, col3, col4, col5, _ = st.columns([7, 1, 1, 1, 1, 1, 7])
+else:
+    raise ValueError("Too many clicks")
+
+_, col1, col2, col3, col4, col5, _ = st.columns([4, 1, 1, 1, 1, 2, 4])
 
 col1.write(f"# {first}")
-col2.write(f"# +")
+col2.write("# +")
 col3.write(f"# {second}")
 col4.write("# =")
+
+
+@st.cache_data
+def img_exists(url):
+    if url == "NO MATCH":
+        return False
+    try:
+        requests.head(url).raise_for_status()
+        return True
+    except RequestException:
+        return False
+
+
 if third == "?":
     col5.write(f"# {third}")
 else:
-    try:
-        requests.head(third).raise_for_status()
+    if img_exists(third):
         col5.image(third, width=70)
-    except RequestException:
-        col5.write(f"## Not found")
+    else:
+        col5.write("## ❌")
 
-for idx, point in enumerate(points):
-    emoji = code_point_to_emoji(point)
-    if st.button(emoji):
-        st.session_state["clicked"].append(point)
-        if len(st.session_state["clicked"]) > 2:
-            st.session_state["clicked"].pop(0)
-        st.experimental_rerun()
+
+def button_clicked(point):
+    st.session_state["clicked"].append(point)
+    if len(st.session_state["clicked"]) > 2:
+        st.session_state["clicked"].pop(0)
+
+    st.query_params.update({"clicked": st.session_state["clicked"]})
+
+
+def draw_emoji_grid():
+    with st.container(height=500):
+        st.html("<span class='emoji-grid'></span>")
+        for point in points:
+            emoji = code_point_to_emoji(point)
+            st.button(emoji, on_click=button_clicked, args=(point,))
+
+
+draw_emoji_grid()
